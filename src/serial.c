f85526cb2fe4df4c0a6edad5182c53942f7c6ae8
Henry Shepperd <hshepperd@gmail.com>
2016-01-24 14:40:14 +1100

    Make serial task interrupt driven

--- src/serial.c
+++ src/serial.c
@@ -1,7 +1,8 @@
 #include "serial.h"
 
 static struct {
-    uint8_t currentInputStream;
+    Stream* currentInput;
+    uint8_t nextIndex;
 } mData;
 
 static void setup(Task* task);
@@ -12,7 +13,7 @@ Task serialTask = {
     .dataSize = sizeof(mData),
     .setup = setup,
     .loop = loop,
-    .priority = PRIORITY_DRIVER,
+    .priority = PRIORITY_LOW,
 };
 
 static void setup(Task* task) {
@@ -22,32 +23,42 @@ static void setup(Task* task) {
         .characterSize = USART0_8_BIT,
         .baud = UBRR_VALUE,
         .use2X = USE_2X,
+        .dataRegisterEmptyIntEnable = true,
+        .rxCompleteIntEnable = true,
     };
 
     usart0(config);
 }
 
 static void loop(Task* task) {
-    if(usart0DataRegisterEmpty()) {
-        Stream* stream = task->inputStreams[mData.currentInputStream];
-        uint8_t data;
-
-        if((stream != NULL) && streamPop(stream, &data)) {
-            usart0Push(data);
-        } else {
-            mData.currentInputStream = (mData.currentInputStream + 1) % task->inputStreamCount;
-        }
-    }
-
-    if(usart0ReceiveComplete()) {
-        const uint8_t data = usart0Pop();
-
-        for(uint8_t i = 0; i < task->outputStreamCount; i++) {
-            Stream* stream = task->outputStreams[i];
-
-            if(stream != NULL) {
-                streamPush(stream, data);
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        if(task->inputStreamCount > 0) {
+            if(streamHasData(mData.currentInput)) {
+                usart0DataRegisterEmptyInterruptEnable(true);
+            } else {
+                mData.nextIndex = (mData.nextIndex + 1) % task->inputStreamCount;
+                mData.currentInput = task->inputStreams[mData.nextIndex];
             }
         }
+    }
+}
+
+ISR(USART_UDRE_vect) {
+    uint8_t data;
+
+    if(streamPop(mData.currentInput, &data)) {
+        usart0Push(data);
+    } else {
+        usart0DataRegisterEmptyInterruptEnable(false);
+    }
+}
+
+ISR(USART_RX_vect) {
+    const uint8_t data = usart0Pop();
+
+    for(uint8_t i = 0; i < serialTask.outputStreamCount; i++) {
+        Stream* stream = serialTask.outputStreams[i];
+
+        streamPush(stream, data);
     }
 }
