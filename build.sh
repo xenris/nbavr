81152d83f9267a0089c38a78a9b9ef54e1c601d6
Henry Shepperd <hshepperd@gmail.com>
2016-02-05 13:45:10 +1100

    Add option for debugging using avr-gdb, simavr, and ddd

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 # "build.sh -u hello" to also upload the example "hello".
 
 mcu=m328p
+mmcu=atmega328p
+freq=16000000
 port=/dev/ttyACM0
 programmer=stk500v2
 
@@ -22,6 +24,29 @@ if [ "${1}" == "-u" ]; then
         hexfile="$exampledir/gen/firmware.hex"
 
         avrdude -p $mcu -P $port -c $programmer -e -U flash:w:$hexfile
+    else
+        echo "Example ${2} not found"
+        echo "Try one of:"
+        for e in examples/*/ ; do
+            basename $e
+        done
+    fi
+fi
+
+if [ "${1}" == "-d" ]; then
+    hash avr-gdb 2>/dev/null || { echo >&2 "avr-gdb is needed for debugging but it's not installed."; exit 1; }
+    hash simavr 2>/dev/null || { echo >&2 "simavr is needed for debugging but it's not installed."; exit 1; }
+    hash ddd 2>/dev/null || { echo >&2 "ddd is needed for debugging but it's not installed."; exit 1; }
+
+    exampledir="examples/${2}"
+
+    if [ -d $exampledir ] && [ "${2}" != "" ]; then
+        cd $exampledir
+
+        simavr -g -v -v -t -m $mmcu -f $freq gen/firmware.elf &
+        ddd --debugger "avr-gdb" --eval-command="file gen/firmware.elf" --eval-command="target remote localhost:1234"
+
+        pkill simavr
     else
         echo "Example ${2} not found"
         echo "Try one of:"
--- examples/twi/build.sh
+++ examples/twi/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+mmcu=atmega328p
+freq=16000000
+
 set -e
 
 if [ ! -d ".tup" ]; then
@@ -14,3 +17,13 @@ if [ "${1}" = "-u" ]; then
     avrdude -p m328p -P /dev/ttyACM0 -c stk500v2 -e -U flash:w:gen/firmware.hex
 fi
 
+if [ "${1}" == "-d" ]; then
+    hash avr-gdb 2>/dev/null || { echo >&2 "avr-gdb is needed for debugging but it's not installed."; exit 1; }
+    hash simavr 2>/dev/null || { echo >&2 "simavr is needed for debugging but it's not installed."; exit 1; }
+    hash ddd 2>/dev/null || { echo >&2 "ddd is needed for debugging but it's not installed."; exit 1; }
+
+    simavr -g -v -v -t -m $mmcu -f $freq gen/firmware.elf &
+    ddd --debugger "avr-gdb" --eval-command="file gen/firmware.elf" --eval-command="target remote localhost:1234"
+
+    pkill simavr
+fi
