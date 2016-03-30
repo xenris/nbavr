8c3aa039441da97f6f9be73f6b26f067c6fbf39f
Henry Shepperd <hshepperd@gmail.com>
2016-03-30 12:06:10 +1100

    Fix and tidy the compilation process and remove simavr debugging
    Simavr doesn't seem reliable enough at the moment to actually use for debugging.
    e.g. https://github.com/buserror/simavr/issues/125

--- build.sh
+++ build.sh
@@ -3,17 +3,9 @@
 # "build.sh" to build the library and its examples.
 # "build.sh -u hello" to also upload the example "hello".
 
-mcu=m328p
-mmcu=atmega328p
-freq=16000000
-port=/dev/ttyACM0
-programmer=stk500v2 #use "arduino" to upload to an Arduino device.
-
 set -e
 
-if [ ! -d ".tup" ]; then
-    tup init
-fi
+source ./build.config
 
 tup
 
@@ -24,29 +16,6 @@ if [ "${1}" == "-u" ]; then
         hexfile="$exampledir/gen/firmware.hex"
 
         avrdude -p $mcu -P $port -c $programmer -e -U flash:w:$hexfile
-    else
-        echo "Example ${2} not found"
-        echo "Try one of:"
-        for e in examples/*/ ; do
-            basename $e
-        done
-    fi
-fi
-
-if [ "${1}" == "-d" ]; then
-    hash avr-gdb 2>/dev/null || { echo >&2 "avr-gdb is needed for debugging but it's not installed."; exit 1; }
-    hash simavr 2>/dev/null || { echo >&2 "simavr is needed for debugging but it's not installed."; exit 1; }
-    hash kdbg 2>/dev/null || { echo >&2 "kdbg is needed for debugging but it's not installed."; exit 1; }
-
-    exampledir="examples/${2}"
-
-    if [ -d $exampledir ] && [ "${2}" != "" ]; then
-        cd $exampledir
-
-        simavr -g -v -v -t -m $mmcu -f $freq gen/firmware.elf &
-        kdbg -r localhost:1234 gen/firmware.elf
-
-        pkill simavr
     else
         echo "Example ${2} not found"
         echo "Try one of:"
--- examples/twi/build.sh
+++ examples/twi/build.sh
@@ -1,30 +1,13 @@
 #!/bin/sh
 
-mmcu=atmega328p
-freq=16000000
-programmer=stk500v2 #use "arduino" to upload to an Arduino device.
-
 set -e
 
-if [ ! -d ".tup" ]; then
-    tup init
-fi
+source ./build.config
 
 tup
 
-avr-size gen/firmware.elf -C --mcu=atmega328p
+avr-size gen/firmware.elf -C --mcu=$mmcu
 
 if [ "${1}" = "-u" ]; then
-    avrdude -p m328p -P /dev/ttyACM0 -c $programmer -e -U flash:w:gen/firmware.hex
-fi
-
-if [ "${1}" == "-d" ]; then
-    hash avr-gdb 2>/dev/null || { echo >&2 "avr-gdb is needed for debugging but it's not installed."; exit 1; }
-    hash simavr 2>/dev/null || { echo >&2 "simavr is needed for debugging but it's not installed."; exit 1; }
-    hash kdbg 2>/dev/null || { echo >&2 "kdbg is needed for debugging but it's not installed."; exit 1; }
-
-    simavr -g -v -v -t -m $mmcu -f $freq gen/firmware.elf &
-    kdbg -r localhost:1234 gen/firmware.elf
-
-    pkill simavr
+    avrdude -p $mcu -P $port -c $programmer -e -U flash:w:gen/firmware.hex
 fi
