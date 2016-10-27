c10e5f88daedd720afe65ecd28a9de5f6ba026fd
Henry Shepperd <hshepperd@gmail.com>
2016-10-27 17:36:12 +1100

    Add command to kill picocom before uploading program

--- build.sh
+++ build.sh
@@ -13,6 +13,8 @@ if [ "${1}" == "-u" ]; then
     exampledir="examples/${2}"
 
     if [ -d $exampledir ] && [ "${2}" != "" ]; then
+        pkill picocom && sleep 1 || true
+
         hexfile="$exampledir/gen/firmware.hex"
 
         avrdude -p $mcu -P $port -c $programmer -e -U flash:w:$hexfile
