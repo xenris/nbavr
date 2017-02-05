268871571c4d9d7aa2342af6ffa8c9476e07ba09
Henry Shepperd <hshepperd@gmail.com>
2017-02-05 16:28:34 +1100

    Change some more typecasts to c++ style

--- src/nbavr.hpp
+++ src/nbavr.hpp
@@ -74,11 +74,11 @@ extern "C" force_inline void __cxa_guard_abort (__guard *) __attribute__((used))
 extern "C" force_inline void __cxa_pure_virtual(void) __attribute__((used));
 
 int __cxa_guard_acquire(__guard *g) {
-    return !*(char *)(g);
+    return !*reinterpret_cast<char*>(g);
 }
 
 void __cxa_guard_release(__guard *g) {
-    *(char *)g = 1;
+    *reinterpret_cast<char*>(g) = 1;
 }
 
 void __cxa_pure_virtual(void) {
