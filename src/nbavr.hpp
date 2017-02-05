fea58aa580fdc3f9fe2a2c4a11824057d2366853
Henry Shepperd <hshepperd@gmail.com>
2017-02-05 16:26:13 +1100

    Revert "Remove seemingly unused compiler functions"
    Actually, they do seem to be used. Used with objects declared static.
    
    This reverts commit 8e492b40e6dab86d11b792601c68924b7119556c.

--- src/nbavr.hpp
+++ src/nbavr.hpp
@@ -66,4 +66,25 @@ inline void nbavr(Clock& clock, Task* (&tasks)[S]) {
     }
 }
 
+__extension__ typedef int __guard __attribute__((mode (__DI__)));
+
+extern "C" force_inline int __cxa_guard_acquire(__guard *) __attribute__((used));
+extern "C" force_inline void __cxa_guard_release (__guard *) __attribute__((used));
+extern "C" force_inline void __cxa_guard_abort (__guard *) __attribute__((used));
+extern "C" force_inline void __cxa_pure_virtual(void) __attribute__((used));
+
+int __cxa_guard_acquire(__guard *g) {
+    return !*(char *)(g);
+}
+
+void __cxa_guard_release(__guard *g) {
+    *(char *)g = 1;
+}
+
+void __cxa_pure_virtual(void) {
+}
+
+void __cxa_guard_abort (__guard *) {
+}
+
 #endif
