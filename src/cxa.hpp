bea53b52c7d52b23e46453dadb838bb2790c7eb0
Henry Shepperd <hshepperd@gmail.com>
2017-08-17 14:08:40 +1000

    Move cxa guard functions to own header

---
+++ src/cxa.hpp
@@ -0,0 +1,25 @@
+#ifndef NBAVR_CXA_HPP
+#define NBAVR_CXA_HPP
+
+__extension__ typedef int __guard __attribute__((mode (__DI__)));
+
+extern "C" force_inline int __cxa_guard_acquire(__guard *) __attribute__((used));
+extern "C" force_inline void __cxa_guard_release (__guard *) __attribute__((used));
+extern "C" force_inline void __cxa_guard_abort (__guard *) __attribute__((used));
+extern "C" force_inline void __cxa_pure_virtual(void) __attribute__((used));
+
+int __cxa_guard_acquire(__guard *g) {
+    return !*reinterpret_cast<char*>(g);
+}
+
+void __cxa_guard_release(__guard *g) {
+    *reinterpret_cast<char*>(g) = 1;
+}
+
+void __cxa_pure_virtual(void) {
+}
+
+void __cxa_guard_abort (__guard *) {
+}
+
+#endif
--- src/nbavr.hpp
+++ src/nbavr.hpp
@@ -13,31 +13,11 @@
 #include "clock.hpp"
 #include "taskmanager.hpp"
 #include "print.hpp"
+#include "cxa.hpp"
 
 #include "tasks/lcd.hpp"
 #include "tasks/serial.hpp"
 #include "tasks/servo.hpp"
 #include "tasks/twi.hpp"
-
-__extension__ typedef int __guard __attribute__((mode (__DI__)));
-
-extern "C" force_inline int __cxa_guard_acquire(__guard *) __attribute__((used));
-extern "C" force_inline void __cxa_guard_release (__guard *) __attribute__((used));
-extern "C" force_inline void __cxa_guard_abort (__guard *) __attribute__((used));
-extern "C" force_inline void __cxa_pure_virtual(void) __attribute__((used));
-
-int __cxa_guard_acquire(__guard *g) {
-    return !*reinterpret_cast<char*>(g);
-}
-
-void __cxa_guard_release(__guard *g) {
-    *reinterpret_cast<char*>(g) = 1;
-}
-
-void __cxa_pure_virtual(void) {
-}
-
-void __cxa_guard_abort (__guard *) {
-}
 
 #endif
