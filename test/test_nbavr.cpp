b57d95b20d07d524a4e2f917a8f453f72d0ec763
Henry Shepperd <hshepperd@gmail.com>
2017-08-25 15:31:50 +1000

    Add tests for reverse and itoa functions

--- test/test_nbavr.cpp
+++ test/test_nbavr.cpp
@@ -35,3 +35,56 @@ TEST(Clock, ticksToMillis) {
     EXPECT_EQ((Clock<TimerCounter1, 20000000>::ticksToMillis(312500)), 1000);
     EXPECT_EQ((Clock<TimerCounter1, 20000000>::ticksToMillis(312500)), 1000);
 }
+
+TEST(Util, reverse) {
+    char str[8];
+
+    strcpy(str, "");
+    EXPECT_STREQ(reverse(str, 0), "");
+
+    strcpy(str, "a");
+    EXPECT_STREQ(reverse(str, 1), "a");
+
+    strcpy(str, "ab");
+    EXPECT_STREQ(reverse(str, 2), "ba");
+
+    strcpy(str, "abc");
+    EXPECT_STREQ(reverse(str, 3), "cba");
+
+    strcpy(str, "abc");
+    EXPECT_STREQ(reverse(str, 2), "bac");
+}
+
+TEST(Print, itoa) {
+    char str[64];
+
+    itoa(str, int8_t(0), 10);
+    EXPECT_STREQ(str, "0");
+
+    itoa(str, uint8_t(147), 10);
+    EXPECT_STREQ(str, "147");
+
+    itoa(str, int16_t(1476), 10);
+    EXPECT_STREQ(str, "1476");
+
+    itoa(str, int16_t(-1476), 10);
+    EXPECT_STREQ(str, "-1476");
+
+    itoa(str, int16_t(764), 2);
+    EXPECT_STREQ(str, "1011111100");
+
+    itoa(str, int16_t(764), 1);
+    EXPECT_STREQ(str, "1011111100");
+
+    itoa(str, int16_t(847), 16);
+    EXPECT_STREQ(str, "34f");
+
+    itoa(str, int16_t(-847), 16);
+    EXPECT_STREQ(str, "-34f");
+
+    itoa(str, int32_t(36), 36);
+    EXPECT_STREQ(str, "10");
+
+    itoa(str, int32_t(36), 37);
+    EXPECT_STREQ(str, "10");
+}
