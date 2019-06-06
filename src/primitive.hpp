927179598b6f8e0bbfe2eb49398f57622d1431ee
Henry Shepperd <hshepperd@gmail.com>
2019-06-06 22:30:40 +1000

    Correct some static assertion messages

--- src/primitive.hpp
+++ src/primitive.hpp
@@ -22,14 +22,14 @@ using uint64_t = If<sizeof(unsigned long) == 8, unsigned long, unsigned long lon
 using size_t = If<sizeof(unsigned int) == 2, unsigned int, unsigned long>::Value;
 using ssize_t = If<sizeof(int) == 2, int, long>::Value;
 
-static_assert(sizeof(int8_t) == 1, "int8 is not the right size");
-static_assert(sizeof(int16_t) == 2, "int16 is not the right size");
-static_assert(sizeof(int32_t) == 4, "int32 is not the right size");
-static_assert(sizeof(int64_t) == 8, "int64 is not the right size");
-static_assert(sizeof(uint8_t) == 1, "word8 is not the right size");
-static_assert(sizeof(uint16_t) == 2, "word16 is not the right size");
-static_assert(sizeof(uint32_t) == 4, "word32 is not the right size");
-static_assert(sizeof(uint64_t) == 8, "word64 is not the right size");
+static_assert(sizeof(int8_t) == 1, "int8_t is not the right size");
+static_assert(sizeof(int16_t) == 2, "int16_t is not the right size");
+static_assert(sizeof(int32_t) == 4, "int32_t is not the right size");
+static_assert(sizeof(int64_t) == 8, "int64_t is not the right size");
+static_assert(sizeof(uint8_t) == 1, "uint8_t is not the right size");
+static_assert(sizeof(uint16_t) == 2, "uint16_t is not the right size");
+static_assert(sizeof(uint32_t) == 4, "uint32_t is not the right size");
+static_assert(sizeof(uint64_t) == 8, "uint64_t is not the right size");
 
 template <>
 constexpr bool isInteger<char>() {
