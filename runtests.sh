ce94168b356ab600ab183c73d840854576f43e09
Henry Shepperd <hshepperd@gmail.com>
2020-02-27 22:13:23 +1100

    Add option to list chip tests

--- runtests.sh
+++ runtests.sh
@@ -4,6 +4,8 @@
 # ./runtests.sh
 # Run some tests:
 # ./runtests.sh attiny85 atmega2560
+# List all tests:
+# ./runtests.sh -l
 # Run all but some tests: TODO
 # ./runtests.sh -n attiny85 atmega2560
 # Debug tests: TODO
@@ -19,18 +21,34 @@ cflags="-Werror -Wall -fmax-errors=3 -std=c++17 -g"
 includes="-I ../src/"
 libs="-lgtest"
 
+update=0
+showChips=0
+
 if [ "$1" == "-u" ] ; then
     declare -a args=("${@:2}")
     update=1
+elif [ "$1" == "-l" ] ; then
+    showChips=1
 else
     declare -a args=("${@}")
-    update=0
 fi
 
 if [ ${#args[@]} -eq 0 ] ; then
     declare -a chips=($(basename -a -s .hpp $(find ../src/hardware/chips/ -name '*.hpp')))
 else
     declare -a chips=("$args")
+fi
+
+if [ "$showChips" -eq 1 ] ; then
+    for chip in "${chips[@]}" ; do
+        if [[ "${ignoreList[@]}" =~ "$chip" ]]; then
+            continue
+        fi
+
+        echo "$chip"
+    done
+
+    exit 0
 fi
 
 for chip in "${chips[@]}" ; do
