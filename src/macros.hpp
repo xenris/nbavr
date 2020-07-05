0d14fc0bacc589c7a0e3845f06dfa3d2b1fc0e82
Henry Shepperd <hshepperd@gmail.com>
2020-07-05 17:31:59 +1000

    Change REG_DEFINED macro to work with more complex register definitions
    Note that it will no longer work if the register is at address 0.

--- src/macros.hpp
+++ src/macros.hpp
@@ -220,9 +220,10 @@
 
 /// #### REG_DEFINED(A)
 /// If A evaluates to a register -> true<br>
-/// e.g. REG_DEFINED(CHIP_REG_ADC) -> true (On devices with that register.)
+/// e.g. REG_DEFINED(CHIP_REG_ADC) -> true (On devices with that register.)<br>
+/// Note: Register at address 0x00 will always return false.
 #define REG_DEFINED(A) REG_DEFINED_(A)
-#define REG_DEFINED_(A) DEFINED(A ## _ADDR)
+#define REG_DEFINED_(A) A ## _ADDR
 
 #define FIRST(A, ...) A
 #define SECOND(A, B, ...) B
